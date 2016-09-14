c1a9b32795421ba1116acb3967fcda413ceeaf87
ErikNovak <novak.erik@gmail.com>
2016-09-14 13:45:36 +0200

    Fixed the service creation bug. Path to node.exe must include the .exe file.

--- dev/CreateService.cmd
+++ dev/CreateService.cmd
@@ -4,13 +4,16 @@ SETLOCAL ENABLEEXTENSIONS
 :: Creates the Service using nssm
 
 :: Set path to node.exe
-nssm install "Job Data Server Service" "C:\Program Files\nodejs"
+nssm install "Job Data Server Service" "C:\Program Files\nodejs\node.exe"
 
 :: Set path to app directory
 nssm set "Job Data Server Service" AppDirectory "C:\work\edsa\jobanalysis-dataserver"
 
 :: Set app parameters
 nssm set "Job Data Server Service" AppParameters "server.js"
+
+:: Add port to the environment
+nssm set "Job Data Server Service" AppEnvironmentExtra "PORT=2510"
 
 :: start the service
 nssm start "Job Data Server Service"
