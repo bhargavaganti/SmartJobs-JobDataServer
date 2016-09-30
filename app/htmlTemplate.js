aa5a3baf40227dafa55ea7274fbf3e04d208be73
ErikNovak <novak.erik@gmail.com>
2016-09-30 11:00:12 +0200

    Changed mustache format to hbs.

--- app/htmlTemplate.js
+++ app/htmlTemplate.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path = require('path');
 
 // template path
-var filePath = path.join(__dirname, '../data/vl-template/videolectures-edsa-jobs.stache');
+var filePath = path.join(__dirname, '../data/vl-template/videolectures-edsa-jobs.hbs');
 var htmlRender = function (options) {
     var data = fs.readFileSync(filePath, { encoding: 'utf-8' });
     return Mustache.render(data, options);
